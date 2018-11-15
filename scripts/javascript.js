bfa2cac15147d65508d34082d802914f1e7a08d7
Ella <ellaelonen@gmail.com>
2018-11-15 18:52:09 +0100

    add IE8-compatibility

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,5 @@
 
+
 function print_cookie_monster(){
     var firstName = 'Cookie';
     var lastName = 'monster';
@@ -15,29 +16,32 @@ function welcome(){
     document.write('<p> Welcome, ' + name + '</p>');
 }
 
-
 function collect_items(){
     var items = [];
     var input = prompt("What do you want to add to the array of items?")
 
     items.unshift('input') //Adds item to the beginning. Push adds to the end.
     document.write('<p>items contains: ' + items + ' '  + + items[items.length-1] + ' ' + input + '</p>');
+    for (var i in items){
+        document.write(items[i]);
+    }
 }
 
-function print_numbers(){
-    
-}
-
-function print2DArray(){
-    var ar = [[3, 2, 1], [5, 1, 6], [1, 6, 0], [4, 7, 8], [4, 15, 2, 8]];
+function print_2D_array(){
+    var ar = [[3, 2, 1], [5, 1, 6], [1, 6, 0], [4, 7, 8], [4, 15, 2]];
  
     for (var i in ar) 
     {
        document.write("row " + i); //console.log 
        for (var j in ar[i]) 
          {
-          document.write(" " + a[i][j]);
+          document.write(" " + ar[i][j] + " ");
          }
     }
+
+    function print_message(){
+        var message = "Array contains" + input;
+    }
 }
+
 
