b105ba44790286297fe8882f53847cdf77cf1d4e
Ella <ellaelonen@gmail.com>
2018-12-08 15:46:23 +0100

    add javascript intro

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,5 +1,4 @@
 
-
 function print_welcome_message(){
 	var welcome = "hello";
     var name = reader;
@@ -16,34 +15,69 @@ function welcome(){
     document.write('<p> Welcome, ' + name + '</p>');
 }
 
-function collect_items(){
-    var items = [];
+function print_array(){
+    var items = ["item1", "item2"];
     var input = prompt("What do you want to add to the array of items?");
 
-    items.unshift('input'); //Adds item to the beginning. Push adds to the end.
-    document.write('<p>items contains:</p>');
+    items.push(input); //Adds item to the beginning. Unshift adds to the beginning.
+    /*document.write('<p>items contains:</p>');
     for (var i in items){
         document.write(items[i]);
-    }
+    }*/
+    document.getElementById("arr").innerHTML = items;
 }
 
+//not used
 function print_2D_array(){
     var ar = [[3, 2, 1], [5, 1, 6], [1, 6, 0], [4, 7, 8], [4, 15, 2]];
  
-    for (var i in ar) 
-    {
-       document.write("row " + i); //console.log 
-       for (var j in ar[i]) 
-         {
+    for (var i in ar) {
+       document.write("row " + i); 
+       for (var j in ar[i]){
           document.write(" " + ar[i][j] + " ");
          }
     }
+} 
 
  function print_message(){
-     var message = "Array contains" + input;
+     var mess = "Hello, this is a String.";
+     document.getElementById("message").innerHTML = mess;
+     var node = document.createTextNode(" This is also a String, appended to the previous one.");
+     message.appendChild(node);
     }
 	
 function print_date(){
 	var d = new Date();
-	document.getElementById("demo").innerHTML = d;
+	document.getElementById("date").innerHTML = d;
 }
+    
+function print_number(){
+	var num = 444444444444444;
+	document.getElementById("number").innerHTML = num;
+}
+
+function print_boolean(){
+    var boo = true;
+    document.getElementById("boolean").innerHTML = boo;
+}
+
+function print_math(){
+    var p = Math.PI;
+    document.getElementById("math").innerHTML = p;
+}
+
+function print_reg(){
+    var str = "This is my Webproject!!!";
+    var RegEx = /WebProject/i;
+    var sub = RegEx.exec(str);
+    document.getElementById("reg").innerHTML = "RegExp: " + RegEx.toString() + " " + "Search result found in the text 'This is my Webproject!!!': "  + sub;
+}
+
+
+function print_global(){
+    var inf = 1 / 0;
+    var n = "string";
+    document.getElementById("glob").innerHTML = "Result of calculation: " + inf + "<br>" + "Type of variable: " + typeof(nan) + "<br>" +
+    "Is variable NaN?: " + isNaN(inf) + "<br>" + "Is the string 'string' a NaN?: " + isNaN(n);
+}
+
